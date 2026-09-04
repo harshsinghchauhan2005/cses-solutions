bf957361dd1a06cb25a1811fd7040759f7306582
Harsh Singh Chauhan <hsking.hh@gmail.com>
2026-09-04 18:30:54 +0530

    Solve: Weird Algorithm

---
+++ weird_algo.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve(){
+    ll n;
+    cin>>n;
+
+    while(n!=1){
+        if(n%2==0) n/=2;
+        else {
+            n=(n*3)+1;
+        }
+
+        cout<<n<<" ";
+    }
+
+    cout<<'\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
